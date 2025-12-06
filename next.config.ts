562f6197b99540997d84b94823b6bcb7b97e9ef0
Esra Oğuz <esraoguz@Esra-MacBook-Air.local>
2025-12-06 21:49:43 +0300

    Settings for vercel deploy have been updated

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/kadir-oguz-elektrik', // GitHub repository adınız buraya gelmeli
   images: {
-    unoptimized: true, // GitHub Pages statik export için gereklidir
     remotePatterns: [
       {
         protocol: "https",
